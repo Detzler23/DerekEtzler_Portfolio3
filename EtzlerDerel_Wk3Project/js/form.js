d93d042af18d86c3070929ba9d1b8f4e77eb9a55
Derek Etzler <derek.etzler@student.fullsail.edu>
2020-03-23 11:24:18 -0400

    Reworked week 3 Assignment
    Fixed the issues I had with this assignment to get everything working.

--- EtzlerDerel_Wk3Project/js/form.js
+++ EtzlerDerel_Wk3Project/js/form.js
@@ -1,11 +1,24 @@
-var contactForm = document.querySelector('form');
-//Testing for tommorrow//
+var contactForm = -document.querySelector('form');
+var pTag = document.querySelectorAll('p');
+pTag[0].innerHTML = "Welcome to my contact page! Here you may contact me with your favorite courses you have played, courses you are looking to play, instructor recommendations, etc. " +
+    "The sport of gold is wonderful and brings many people together. Again feel free to " +
+    "contact me with anything!";
 
+var button = document.querySelector('button');
+button.setAttribute('class', 'disabled');
+
+var inputs = document.querySelectorAll('input');
+
+document.getElementsByName('name')[0].placeholder = 'Name';
+document.getElementsByName('phone')[0].type = 'text';
+document.getElementsByName('message')[0].placeholder = "Lets Hear From You!";
+
+var contactForm = document.querySelector('form');
 
 function validateForm(event) {
     event.preventDefault();
 
-    var form = document.querySelector('text');
+    var form = document.querySelector('form');
     var fields = form.querySelectorAll('.required');
     var valid = true;
     for (var i = 0; i < fields.length; i++) {
@@ -35,8 +48,8 @@ function validateRequired(event) {
 
 var contactFields = contactForm.querySelectorAll('.required');
 for (let i = 0; i < contactFields.length; i++) {
-    contactFields[i].addEventListener("input", validateForm);
-    contactFields[i].addEventListener("blur", validateRequired);
+    contactFields[i].addEventListener('input', validateForm);
+    contactFields[i].addEventListener('blur', validateRequired);
 }
 
 function send(event) {
@@ -45,16 +58,11 @@ function send(event) {
     var message = '<h2>Thank You!</h2><p> Thanks for contacting me! The game of golf is a great way to enjoy the outdoors, exercise and have fun! </p>';
 
     var target = event.target;
-    var disabled = target.classList.contains('.disabled');
+    var disabled = target.classList.contains('disabled');
 
     if (disabled === false) {
         form.innerHTML = message;
     }
-    /*else {
-        document.getElementById('button').disabled = false;
-    }*/
 }
-
-
 var submit = contactForm.querySelector('[type=submit]');
 submit.addEventListener('click', send);
